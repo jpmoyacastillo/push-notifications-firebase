62aefac35f3ec74b93f7ae34510fc966e83a2f58
JP <juanpablo@rindegastos.com>
2022-05-11 19:09:48 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.coglocal.devpush',
+  appName: 'pushApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
